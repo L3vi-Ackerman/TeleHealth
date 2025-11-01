5b6ec898bdfbb84983dce177143d86b4e470e0c0
L3vi Ackerman <l3vi3211@gmail.com>
2025-11-01 15:46:08 +0545

    fix: bug

--- src/routes/_app/patients/$id/index.tsx
+++ src/routes/_app/patients/$id/index.tsx
@@ -2,26 +2,28 @@ import { useGetReport } from '@/hooks/report/queries'
 import { ReportCard } from '@/components/reports/card'
 import { createFileRoute } from '@tanstack/react-router'
 import { ReportDialog } from '@/components/reports/dialog'
+
 export const Route = createFileRoute('/_app/patients/$id/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const params = Route.useParams()
-  const { id } = params
+  const { id } = Route.useParams()
   const { data } = useGetReport(id)
+
   console.log(data)
+
   return (
     <div>
-      {' '}
       <div className="flex flex-col sm:flex-row justify-between items-center border-b pb-4 mb-6">
         <h1 className="text-4xl font-extrabold bg-primary bg-clip-text text-transparent tracking-tight">
           Reports
         </h1>
         <div className="mt-4 sm:mt-0">
           <ReportDialog />
-        </div
+        </div>
       </div>
+
       <div className="p-4">
         {data?.data?.results?.map((r) => (
           <ReportCard key={r.id} report={r} />
